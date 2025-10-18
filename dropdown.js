397031d6c09c0bbe51dc6f9738cf672d5215fd3d
grily <140758522+grilymannen@users.noreply.github.com>
2025-10-18 18:44:56 +0200

    Add files via upload

--- dropdown.js
+++ dropdown.js
@@ -1,19 +1,19 @@
 /* When the user clicks on the button,
 toggle between hiding and showing the dropdown content */
-function toggleDropdown() {
-  document.getElementById("Mdropdown").classList.toggle("show");
+function toggleDropdown(event) {
+  event.stopPropagation(); // Prevent the click from bubbling to window
+  document.getElementById("dropdown").classList.toggle("show");
 }
 
 // Close the dropdown menu if the user clicks outside of it
-window.onclick = function(event) {
-  if (!event.target.matches('.dropdown-button')) {
-    var dropdowns = document.getElementsByClassName("dropdown-content");
-    var i;
-    for (i = 0; i < dropdowns.length; i++) {
-      var openDropdown = dropdowns[i];
-      if (openDropdown.classList.contains('show')) {
-        openDropdown.classList.remove('show');
+window.onclick = function (event) {
+  if (!event.target.matches(".dropdown-button")) {
+    let dropdowns = document.getElementsByClassName("dropdown-content");
+    for (let i = 0; i < dropdowns.length; i++) {
+      let openDropdown = dropdowns[i];
+      if (openDropdown.classList.contains("show")) {
+        openDropdown.classList.remove("show");
       }
     }
   }
-} 
+};
